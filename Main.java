2424a63ad26b249e895d12c57c67cd126b75619c
Gustaf Gunér <gustaf@guner.se>
2019-01-23 11:39:46 +0100

    LC -> LIC

--- Main.java
+++ Main.java
@@ -5,7 +5,7 @@ public class Main {
   private double[] X = new double[100];
   private double[] Y = new double[100];
   private int numpoints = 0;
-  private CONNECTORS[][] LCM = new CONNECTORS[15][15];
+  private CONNECTORS[][] LICM = new CONNECTORS[15][15];
   private boolean[] PUV = new boolean[15];
 
   private boolean[] CVM = new boolean[15];
@@ -24,105 +24,105 @@ public class Main {
     LT, EQ, GQ
   };
 
-  // Returns true if LC0 is true
-  public boolean LC0() {
+  // Returns true if LIC0 is true
+  public boolean LIC0() {
     return false;
   }
 
-  // Returns true if LC1 is true
-  public boolean LC1() {
+  // Returns true if LIC1 is true
+  public boolean LIC1() {
     return false;
   }
 
-  // Returns true if LC2 is true
-  public boolean LC2() {
+  // Returns true if LIC2 is true
+  public boolean LIC2() {
     return false;
   }
 
-  // Returns true if LC3 is true
-  public boolean LC3() {
+  // Returns true if LIC3 is true
+  public boolean LIC3() {
     return false;
   }
 
-  // Returns true if LC4 is true
-  public boolean LC4() {
+  // Returns true if LIC4 is true
+  public boolean LIC4() {
     return false;
   }
 
-  // Returns true if LC5 is true
-  public boolean LC5() {
+  // Returns true if LIC5 is true
+  public boolean LIC5() {
     return false;
   }
 
-  // Returns true if LC6 is true
-  public boolean LC6() {
+  // Returns true if LIC6 is true
+  public boolean LIC6() {
     return false;
   }
 
-  // Returns true if LC7 is true
-  public boolean LC7() {
+  // Returns true if LIC7 is true
+  public boolean LIC7() {
     return false;
   }
 
-  // Returns true if LC8 is true
-  public boolean LC8() {
+  // Returns true if LIC8 is true
+  public boolean LIC8() {
     return false;
   }
 
-  // Returns true if LC9 is true
-  public boolean LC9() {
+  // Returns true if LIC9 is true
+  public boolean LIC9() {
     return false;
   }
 
-  // Returns true if LC10 is true
-  public boolean LC10() {
+  // Returns true if LIC10 is true
+  public boolean LIC10() {
     return false;
   }
 
-  // Returns true if LC11 is true
-  public boolean LC11() {
+  // Returns true if LIC11 is true
+  public boolean LIC11() {
     return false;
   }
 
-  // Returns true if LC12 is true
-  public boolean LC12() {
+  // Returns true if LIC12 is true
+  public boolean LIC12() {
     return false;
   }
 
-  // Returns true if LC13 is true
-  public boolean LC13() {
+  // Returns true if LIC13 is true
+  public boolean LIC13() {
     return false;
   }
 
-  // Returns true if LC14 is true
-  public boolean LC14() {
+  // Returns true if LIC14 is true
+  public boolean LIC14() {
     return false;
   }
 
-  // Set CMV[i] = true if LC i == true
-  public void calculateCMV() {
-    CMV[0] = LC0();
-    CMV[1] = LC1();
-    CMV[2] = LC2();
-    CMV[3] = LC3();
-    CMV[4] = LC4();
-    CMV[5] = LC5();
-    CMV[6] = LC6();
-    CMV[7] = LC7();
-    CMV[8] = LC8();
-    CMV[9] = LC9();
-    CMV[10] = LC10();
-    CMV[11] = LC11();
-    CMV[12] = LC12();
-    CMV[13] = LC13();
-    CMV[14] = LC14();
+  // Set CMV[i] = true if LIC i == true
+  public void caLICulateCMV() {
+    CMV[0] = LIC0();
+    CMV[1] = LIC1();
+    CMV[2] = LIC2();
+    CMV[3] = LIC3();
+    CMV[4] = LIC4();
+    CMV[5] = LIC5();
+    CMV[6] = LIC6();
+    CMV[7] = LIC7();
+    CMV[8] = LIC8();
+    CMV[9] = LIC9();
+    CMV[10] = LIC10();
+    CMV[11] = LIC11();
+    CMV[12] = LIC12();
+    CMV[13] = LIC13();
+    CMV[14] = LIC14();
   };
 
-  public void calculatePUM() {
-    // Matrix operations between CMV and LCM to get PUM
+  public void caLICulatePUM() {
+    // Matrix operations between CMV and LICM to get PUM
   }
 
-  public void calculateFUV() {
+  public void caLICulateFUV() {
     // PUM (***) PUV -> FUV
   }
 
@@ -132,9 +132,9 @@ public class Main {
   }
 
   public void decide() {
-    calculateCMV();
-    calculatePUM();
-    calculateFUV();
+    caLICulateCMV();
+    caLICulatePUM();
+    caLICulateFUV();
 
     if (checkFUV()) {
       System.out.println("yes");
