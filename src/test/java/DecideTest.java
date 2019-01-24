7b4f7c33c19dfb2ae69931bca72c3340346c3d07
Emelie Tham <tham.emelie@gmail.com>
2019-01-24 20:26:17 +0100

    Fix #6 Add logic and unit test for LIC2 See issue #6 for more details.

--- src/test/java/DecideTest.java
+++ src/test/java/DecideTest.java
@@ -3,6 +3,40 @@ import org.junit.Before;
 import org.junit.Test;
 
 public class DecideTest {
+  @Test
+  public void testLIC2() {
+    Decide decide = new Decide();
+
+    //No inputs
+    assertFalse(decide.LIC2());
+
+    //Test three points
+    decide.parameters.EPSILON1 = 4;
+    decide.numpoints = 3;
+    decide.X[0] = 1; decide.Y[0] = 2;
+    decide.X[1] = 2; decide.Y[1] = 2;
+    decide.X[2] = 2; decide.Y[2] = 3;
+    assertFalse(decide.LIC2());
+
+    decide.parameters.EPSILON1 = -1;
+    assertFalse(decide.LIC2());
+
+    //Test four points
+    decide.parameters.EPSILON1 = 2;
+    decide.numpoints = 4;
+    decide.X[0] = 2; decide.Y[0] = 2;
+    decide.X[1] = 2; decide.Y[1] = 2;
+    decide.X[2] = 2; decide.Y[2] = 3;
+    decide.X[3] = 4; decide.Y[3] = 4;
+    assertFalse(decide.LIC2());
+
+    decide.parameters.EPSILON1 = 0;
+    decide.X[0] = 1; decide.Y[0] = 2;
+    decide.X[1] = 2; decide.Y[1] = 2;
+    decide.X[2] = 2; decide.Y[2] = 3;
+    decide.X[3] = 4; decide.Y[3] = 4;
+    assertTrue(decide.LIC2());
+  }
 
   @ Test
   public void testLIC3() {
