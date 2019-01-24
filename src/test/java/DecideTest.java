3be22fcc9f2b2de21429efd543c1e783883b3250
Sasha <sasha.hellstenius@gmail.com>
2019-01-24 16:34:27 +0100

    resolve comments Change indentation to 2 spaces

--- src/test/java/DecideTest.java
+++ src/test/java/DecideTest.java
@@ -4,28 +4,28 @@ import org.junit.Test;
 
 public class DecideTest {
 
-   	@ Test
-	public void testLIC3() {
-	Decide decide = new Decide();
+  @ Test
+  public void testLIC3() {
+    Decide decide = new Decide();
 
-	// No input points 
-	assertFalse(decide.LIC3());
+    // No input points
+    assertFalse(decide.LIC3());
 
-	// Test with three points that are all the same
+    // Test with three points that are all the same
     decide.parameters.AREA1 = 1;
     decide.X[0] = 1; decide.X[1] = 1; decide.X[2] = 1;
     decide.Y[0] = 1; decide.Y[1] = 1; decide.Y[2] = 1;
     decide.numpoints = 3;
-	assertFalse(decide.LIC3());
+    assertFalse(decide.LIC3());
 
-	// // Test with four points 
+    // // Test with four points 
     decide.parameters.AREA1 = 0.5;
     decide.X[0] = 0; decide.X[1] = 1; decide.X[2] = 1; decide.X[3] = 0;
     decide.Y[0] = 0; decide.Y[1] = 1; decide.Y[2] = 0; decide.Y[3] = 1;
     decide.numpoints = 4;
-	assertFalse(decide.LIC3());
+    assertFalse(decide.LIC3());
 
-	decide.parameters.AREA1 = 0.49;
-	assertTrue(decide.LIC3());
-	}
+    decide.parameters.AREA1 = 0.49;
+    assertTrue(decide.LIC3());
+  }
 }
