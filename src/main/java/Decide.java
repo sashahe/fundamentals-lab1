0bc6f29cec953849368368daae58c576237036c5
Gustaf Gunér <gustaf@guner.se>
2019-01-25 10:36:12 +0100

    Remove unused variables

--- src/main/java/Decide.java
+++ src/main/java/Decide.java
@@ -28,7 +28,6 @@ public class Decide {
   // There exists at least one set of two consecutive data points that are a distance greater than
   // the length, LENGTH1, apart.
   public boolean LIC0() {
-    double Ax, Ay, Bx, By;
     for (int i = 0; i < numpoints - 1; i++) {
       double distance = calculateDistance(i, i + 1);
 
