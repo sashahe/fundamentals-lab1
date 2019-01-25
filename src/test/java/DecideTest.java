e058444f049c23eff99c6e83d69bea794f9a7388
Vera Blomkvist Karlsson <verakar@kth.se>
2019-01-25 12:03:28 +0100

    Add unit tests for LIC5

--- src/test/java/DecideTest.java
+++ src/test/java/DecideTest.java
@@ -18,7 +18,7 @@ public class DecideTest {
     decide.numpoints = 3;
     assertFalse(decide.LIC3());
 
-    // // Test with four points 
+    // // Test with four points
     decide.parameters.AREA1 = 0.5;
     decide.X[0] = 0; decide.X[1] = 1; decide.X[2] = 1; decide.X[3] = 0;
     decide.Y[0] = 0; decide.Y[1] = 1; decide.Y[2] = 0; decide.Y[3] = 1;
@@ -28,4 +28,35 @@ public class DecideTest {
     decide.parameters.AREA1 = 0.49;
     assertTrue(decide.LIC3());
   }
+
+  @Test
+  public void testLIC5() {
+    Decide decide = new Decide();
+
+    // No input points should return false
+    assertFalse(decide.LIC5());
+
+    // X[i+1] - X[i] > 0: LIC5 should return false
+    decide.numpoints = 2;
+    decide.X[0] = 0; decide.X[1] = 1;
+    decide.Y[0] = 0; decide.Y[1] = 1;
+    assertFalse(decide.LIC5());
+
+    // X[i+1] - X[i] = 0: LIC5 should return false
+    decide.X[0] = 1; decide.X[1] = 1;
+    decide.Y[0] = 1; decide.Y[1] = 1;
+    assertFalse(decide.LIC5());
+
+    // X[i+1] - X[i] < 0: LIC5 should return true
+    decide.X[0] = 1; decide.X[1] = 0;
+    decide.Y[0] = 0; decide.Y[1] = 1;
+    assertTrue(decide.LIC5());
+
+    // Any consecutive points should work
+    decide.numpoints = 4;
+    decide.X[0] = 1; decide.X[1] = 2; decide.X[2] = 3; decide.X[3] = 0;
+    decide.Y[0] = 0; decide.Y[1] = 1; decide.Y[2] = 1; decide.Y[3] = 0;
+    assertTrue(decide.LIC5());
+  }
+
 }
