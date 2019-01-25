8955623da54eaba3ce72e824034bfc25f568ffc1
Adibbin Haider <adibbin@kth.se>
2019-01-25 09:09:03 +0100

    Add test for LIC1

--- src/test/java/DecideTest.java
+++ src/test/java/DecideTest.java
@@ -5,6 +5,32 @@ import org.junit.Test;
 public class DecideTest {
 
   @ Test
+  public void testLIC1() {
+    Decide d = new Decide();
+    
+    // No input points
+    assertFalse(d.LIC1());
+
+    d.parameters.RADIUS1 = 1;
+    d.numpoints = 3;
+
+    // Test with three points that are all the same
+    d.X[0] = 1; d.X[1] = 1; d.X[2] = 1;
+    d.Y[0] = 1; d.Y[1] = 1; d.Y[2] = 1;
+    assertFalse(d.LIC1());  
+
+    // Test valid input
+    d.X[0] = -1; d.X[1] = 0; d.X[2] = 1;
+    d.Y[0] = 0;  d.Y[1] = 0; d.Y[2] = 1;
+    assertTrue(d.LIC1());
+
+    // Test invalid input
+    d.X[0] = -0.5; d.X[1] = 0; d.X[2] = 0.25;
+    d.Y[0] = 0;    d.Y[1] = 0; d.Y[2] = 0.25;
+    assertFalse(d.LIC1());
+  }
+
+  @ Test
   public void testLIC3() {
     Decide decide = new Decide();
 
